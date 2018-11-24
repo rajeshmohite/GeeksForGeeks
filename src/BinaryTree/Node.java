d3a1bc9bb2fc4b9c0f2872d23e4a1bde8d2d1eed
rajeshmohite <rajehmohite694@gmail.com>
2018-11-24 17:08:59 +0530

    Add Binary Tree Node class

---
+++ src/BinaryTree/Node.java
@@ -0,0 +1,14 @@
+package BinaryTree;
+
+public class Node {
+	int data;
+	Node left;
+	Node right;
+
+	public Node(int data) {
+		super();
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
