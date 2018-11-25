2ee645b31f9986703741a934bc0391ad58a76f4a
rajeshmohite <rajehmohite694@gmail.com>
2018-11-25 16:57:37 +0530

    Add Node.java

---
+++ src/BinarySearchTree/Node.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class Node {
+	int key;
+	Node left;
+	Node right;
+
+	public Node(int key) {
+		super();
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+}
