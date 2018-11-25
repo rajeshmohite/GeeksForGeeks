03752c790729c8d7fe1fe84ccad2829c54f90b8a
rajeshmohite <rajehmohite694@gmail.com>
2018-11-25 19:00:00 +0530

    Add Delete in BST

--- src/BinarySearchTree/_01_BinarySearchTree.java
+++ src/BinarySearchTree/_01_BinarySearchTree.java
@@ -10,10 +10,13 @@ public class _01_BinarySearchTree {
 
 	public static void main(String[] args) {
 		_01_BinarySearchTree tree = new _01_BinarySearchTree();
-
-		/*
-		 * Let us create following BST 50 / \ 30 70 / \ / \ 20 40 60 80
-		 */
+/* Let us create following BST 
+        50 
+     /     \ 
+    30      70 
+   /  \    /  \ 
+  20   40  60   80 
+*/
 		tree.insert(50);
 		tree.insert(30);
 		tree.insert(20);
@@ -22,7 +25,22 @@ public class _01_BinarySearchTree {
 		tree.insert(60);
 		tree.insert(80);
 
-		// print inorder traversal of the BST
+		System.out.println("Inorder traversal of the given tree");
+		tree.inorder(tree.root);
+
+		System.out.println("\nDelete 20");
+		tree.deleteKey(20);
+		System.out.println("Inorder traversal of the modified tree");
+		tree.inorder(tree.root);
+
+		System.out.println("\nDelete 30");
+		tree.deleteKey(30);
+		System.out.println("Inorder traversal of the modified tree");
+		tree.inorder(tree.root);
+
+		System.out.println("\nDelete 50");
+		tree.deleteKey(50);
+		System.out.println("Inorder traversal of the modified tree");
 		tree.inorder(tree.root);
 
 	}
@@ -49,6 +67,52 @@ public class _01_BinarySearchTree {
 			System.out.print(root.key + " ");
 			inorder(root.right);
 		}
+	}
+
+	// This method mainly calls deleteRec()
+	void deleteKey(int key) {
+		root = deleteRec(root, key);
+	}
+
+	/* A recursive function to insert a new key in BST */
+	Node deleteRec(Node root, int key) {
+		/* Base Case: If the tree is empty */
+		if (root == null)
+			return root;
+
+		/* Otherwise, recur down the tree */
+		if (key < root.key)
+			root.left = deleteRec(root.left, key);
+		else if (key > root.key)
+			root.right = deleteRec(root.right, key);
+
+		// if key is same as root's key, then This is the node
+		// to be deleted
+		else {
+			// node with only one child or no child
+			if (root.left == null)
+				return root.right;
+			else if (root.right == null)
+				return root.left;
+
+			// node with two children: Get the inorder successor (smallest
+			// in the right subtree)
+			root.key = minValue(root.right);
+
+			// Delete the inorder successor
+			root.right = deleteRec(root.right, root.key);
+		}
+
+		return root;
+	}
+
+	int minValue(Node root) {
+		int minv = root.key;
+		while (root.left != null) {
+			minv = root.left.key;
+			root = root.left;
+		}
+		return minv;
 	}
 
 }
