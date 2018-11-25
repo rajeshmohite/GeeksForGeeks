492b6dcc7d58363c412edb12b584ec45effce6c0
rajeshmohite <rajehmohite694@gmail.com>
2018-11-25 16:58:23 +0530

    Add insert and inorder traversal operation

---
+++ src/BinarySearchTree/_01_BinarySearchTree.java
@@ -0,0 +1,54 @@
+package BinarySearchTree;
+
+public class _01_BinarySearchTree {
+
+	Node root;
+
+	public _01_BinarySearchTree() {
+		this.root = null;
+	}
+
+	public static void main(String[] args) {
+		_01_BinarySearchTree tree = new _01_BinarySearchTree();
+
+		/*
+		 * Let us create following BST 50 / \ 30 70 / \ / \ 20 40 60 80
+		 */
+		tree.insert(50);
+		tree.insert(30);
+		tree.insert(20);
+		tree.insert(40);
+		tree.insert(70);
+		tree.insert(60);
+		tree.insert(80);
+
+		// print inorder traversal of the BST
+		tree.inorder(tree.root);
+
+	}
+
+	private void insert(int key) {
+		root = insertRec(root, key);
+	}
+
+	private Node insertRec(Node root, int key) {
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
+		if (key < root.key)
+			root.left = insertRec(root.left, key);
+		else if (key > root.key)
+			root.right = insertRec(root.right, key);
+		return root;
+	}
+
+	private void inorder(Node root) {
+		if (root != null) {
+			inorder(root.left);
+			System.out.print(root.key + " ");
+			inorder(root.right);
+		}
+	}
+
+}
